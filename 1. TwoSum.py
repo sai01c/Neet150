bb1a8c68d47eccc2725a9eb4c55c43a2634461b5
Sai Shashank Nayabu <saishashank10@gmail.com>
2022-04-09 13:38:54 -0700

    add

--- 1. TwoSum.py
+++ 1. TwoSum.py
@@ -13,6 +13,13 @@ Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
 
+EXPLANATION: Approach is to store the index, value of the array in a dicitionary. 
+Iterate through the array and check the difference is in dictionary. 
+If the difference is in dictionary, return that particular index and the iteration index. 
+
+TC: we are iterating only once so it is O(n)
+SC: we are using a dictionary so it is O(n)
+
 """
 
 
