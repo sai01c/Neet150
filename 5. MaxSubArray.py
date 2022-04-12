478f70c6dea51a3790cfc38e075f8bd2f4cc2ff5
Sai Shashank Nayabu <saishashank10@gmail.com>
2022-04-12 16:49:11 -0700

    add

--- 5. MaxSubArray.py
+++ 5. MaxSubArray.py
@@ -9,5 +9,22 @@ Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
 
 we need to iterate through the array and add elements to find the subarray sum. 
-only case the sum of the subarray can be decreasing is ihe sum goes negative, so if the sum is negative we make it 0.
+only case the sum of the subarray can be decreasing is the sum goes negative, so if the sum is negative we make it 0.
+
+TC: we are iterating only once so O(n)
+Sc: O(1) no additional space is required. 
 """
+
+
+def maxSum(nums):
+    ans = nums[0]
+    runSum = 0
+    for num in nums:
+        if runSum < 0:
+            runSum = 0
+        runSum += num
+        ans = max(ans, runSum)
+    return ans
+
+
+print(maxSum([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
