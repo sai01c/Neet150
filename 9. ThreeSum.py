84fc2fe2735f864850e6806a774c76e942e13cf9
Sai Shashank Nayabu <saishashank10@gmail.com>
2022-04-12 20:08:55 -0700

    add

---
+++ 9. ThreeSum.py
@@ -0,0 +1,44 @@
+"""
+https://leetcode.com/problems/3sum/
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Example 2:
+
+Input: nums = []
+Output: []
+
+Approach: first sort the numbers use the first number and iterate through the rest of the elements using two pointers. 
+now repeat the same process for the rest of the first elements.
+
+TC: O(n^2)
+SC: O(n)
+
+"""
+
+
+def threeSum(nums):
+    nums.sort()
+    res = []
+    for index, first_val in enumerate(nums):
+        if index > 0 and nums[index] == nums[index-1]:
+            continue
+        left = index + 1
+        right = len(nums) - 1
+        while left < right:
+            summ = first_val + nums[left] + nums[right]
+            if summ < 0:
+                left += 1
+            elif summ > 0:
+                right -= 1
+            else:
+                res.append([first_val, nums[left], nums[right]])
+                left += 1
+                while nums[left] == nums[left-1] and left < right:
+                    left += 1
+    return res
+
+
+print(threeSum([-1, 0, 1, 2, -1, -4]))
--- Takeaways.md
+++ Takeaways.md
@@ -3,3 +3,4 @@
 - Counter is basically a dictionary. In python, dictionary doesn't store the order. So, if you want to sort them copy to a list while sorting it. `sortedCount = sorted(dict.items(), key = lambda x: x[1], reverse = True)`
 - If the problem is about frequency, use dictionary.
 - If the problem is about duplicate, use set.
+- Sorted input - binary search
