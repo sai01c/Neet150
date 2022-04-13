43be2949e74e810c2f8afcdb81dfffd4e70de9c6
Sai Shashank Nayabu <saishashank10@gmail.com>
2022-04-12 20:16:18 -0700

    add

--- 9. ThreeSum.py
+++ 9. ThreeSum.py
@@ -23,11 +23,12 @@ def threeSum(nums):
     nums.sort()
     res = []
     for index, first_val in enumerate(nums):
-        if index > 0 and nums[index] == nums[index-1]:
+        if index > 0 and nums[index] == nums[index-1]:  # to avoid duplicate values
             continue
         left = index + 1
         right = len(nums) - 1
         while left < right:
+            # always compute the equation after the while loop
             summ = first_val + nums[left] + nums[right]
             if summ < 0:
                 left += 1
@@ -36,7 +37,7 @@ def threeSum(nums):
             else:
                 res.append([first_val, nums[left], nums[right]])
                 left += 1
-                while nums[left] == nums[left-1] and left < right:
+                while nums[left] == nums[left-1] and left < right:  # to avoid duplicates
                     left += 1
     return res
 
