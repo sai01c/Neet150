5f887525c156bcc588b450b2c57e529c5fc41b59
Sai Shashank Nayabu <saishashank10@gmail.com>
2022-04-30 08:07:22 -0700

    add

---
+++ 33. ValidPalindrome.py
@@ -0,0 +1,39 @@
+"""
+https://leetcode.com/problems/valid-palindrome/
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Approach: this is a palindrome questions so we have to use two pointers
+we need to eliminate non-alphabetical so increase/decresse the pointers whenever you encounter them. 
+next. we need to check is left == right
+
+TC: O(n) as we are iterating once through the loop
+Sc: O(1) we are using only pointers.
+
+"""
+
+
+def isPalindrome(s):
+    left = 0
+    right = len(s) - 1
+    while left < right:
+        if not s[left].isalnum():  # increasing if the pointer is not alnum
+            left += 1
+        elif not s[right].isalnum():  # decrease the pointer if not alnum
+            right -= 1
+        else:
+            if s[left].lower() != s[right].lower():  # they asked for only lower case letters
+                return False
+            left += 1  # continue increasing the pointers until you complete the while
+            right -= 1
+    return True
+
+
+print(isPalindrome("A man, a plan, a canal: Panama"))
