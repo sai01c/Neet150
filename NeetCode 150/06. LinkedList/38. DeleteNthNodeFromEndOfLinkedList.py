41518becd511373898d3dae8a5ad8dd5cec72ec7
Sai Shashank Nayabu <saishashank10@gmail.com>
2022-07-25 22:18:05 -0700

    add

---
+++ NeetCode 150/06. LinkedList/38. DeleteNthNodeFromEndOfLinkedList.py
@@ -0,0 +1,43 @@
+"""
+https://leetcode.com/problems/remove-nth-node-from-end-of-list/submissions/
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+
+Approach: create two pointers left and right
+idea is to have the diff of n+1 between them
+move right by n times 
+and then move both of them such that right is null
+now remove the node
+
+TC: O(n)
+Sc: O(n)
+"""
+
+
+def __init__(self, val=0, next=None):
+    self.val = val
+    self.next = next
+
+
+def deleteNthNode(head):
+    dummy = ListNode()
+    dummy.next = head
+    left = dummy
+    right = head
+
+    while n > 0 and head:  # move right by n times
+        right = right.next
+        n -= 1
+
+    while right:  # move right to end of the list i.e null
+        right = right.next  # diff bw left and right is n+1
+        left = left.next
+
+    left.next = left.next.next
+    # we can not use return head here. imagine input with just 1 node and we want to remove that.
+    return dummy.next
---
+++ NeetCode 150/06. LinkedList/39. AddTwoNumbers.py
@@ -0,0 +1,31 @@
+"""
+https://leetcode.com/problems/add-two-numbers/
+
+reverse addition - do addition from left to right, you'll understand better
+
+"""
+
+
+def ListNode(self, val, next):
+    self.val = val
+    self.next = next
+
+
+def addTwoNumbers(l1, l2):
+    dummy = ListNode()
+    curr = dummy
+    carry = 0
+    if l1 or l2 or carry:  # edge case if carry is only left over
+        v1 = l1.val if l1 else None
+        v2 = l2.val if l2 else None
+
+        val = v1 + v2 + carry
+        carry = val // 10  # quotient
+        val = val % 10  # reminder
+        curr.next = ListNode(val)
+
+        curr = curr.next  # update pointer
+        l1 = l1.next
+        l2 = l2.next
+
+    return dummy.next
