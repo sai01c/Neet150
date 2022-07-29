9d48828ab431c38aec2096676ca9db133ebe0705
Sai Shashank Nayabu <saishashank10@gmail.com>
2022-07-29 00:01:56 -0700

    add

--- Striver/01. Trees/05. IdenticalTree.py
+++ Striver/01. Trees/05. IdenticalTree.py
@@ -1,24 +1,22 @@
 """
-https://leetcode.com/problems/same-tree/
 
-Approach: 
+Approach: traverse every node and check if p and q are equal
 identify the base cases - if p,q both none that means they are equal
 if one of them is none they are not equal
 if val is not equal they are not equal
 if val is equal then left and right must be equal 
-
 Tc: O(n) as you are travesing every node. 
-Sc: 
 
+you can also use level order traversal for each tree and compare. 
+or any other traversal technique. 
 """
 
 
-def sameBinaryTree(p, q):
-    if not p or not q:  # if p == None or q == None: is not working
-        return False  # what's the difference b/w not p and p == None
+def sameTree(p, q):
     if not p and not q:
         return True
+    if not p or not q:
+        return False
     if p.val != q.val:
         return False
-
-    return (sameBinaryTree(p.left, q.left) and sameBinaryTree(p.right, q.right))
+    return (sameTree(p.left, q.left) and sameTree(p.right, q.right))
