92c443be05ad3d5768740882729958a45bb4fe46
Sai Shashank Nayabu <saishashank10@gmail.com>
2022-08-15 21:15:08 -0700

    add

---
+++ NeetCode 150/Intervals/85. MergeIntervals.py
@@ -0,0 +1,38 @@
+"""
+
+https://leetcode.com/problems/merge-intervals/
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+Approach: First we need to sort them based on the intervals[i][0]. 
+Now, add the first interval and iterate through the rest of them and check if the start is less than end of the last interval
+then try to find the max of those two intervals. 
+else, append the interval to the result. 
+
+TC O(nlogn) as we are sorting here
+SC: O(n) we are using a res array.
+
+"""
+
+
+def mergeIntervals(intervals):
+    intervals.sort(key=lambda x: x[0])
+    res = []
+    res.append(intervals[0])
+    for i in range(1, len(intervals)):
+        lastEnd = res[-1][1]
+        if intervals[i][0] <= lastEnd:
+            res[-1][1] = max(lastEnd, intervals[i][1])
+        else:
+            res.append(intervals[i])
+    return res
+
+
+print(mergeIntervals([[1, 3], [2, 6], [8, 10], [15, 18]]))
---
+++ NeetCode 150/Intervals/86. NonOverlappingIntervals.py
@@ -0,0 +1,36 @@
+"""
+
+https://leetcode.com/problems/non-overlapping-intervals/
+
+Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+Output: 1
+Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+Example 2:
+
+Input: intervals = [[1,2],[1,2],[1,2]]
+Output: 2
+Explanation: You need to remove two [1,2] to make the rest of the intervals non-overlapping.
+
+Approach: this is similar to merge intervals, first you sort the intervals based on start. 
+check if the new interval is less than previous end. 
+then update the prev end based on the condtions
+
+TC: O(nlogn) as we are sorting here
+SC: O(1) creating res variable
+"""
+
+
+def NonOverlap(intervals):
+    intervals.sort(key=lambda x: x[0])
+    prevEnd = intervals[0][1]
+    res = 0
+    for start, end in intervals[1:]:
+        if start < prevEnd:
+            res += 1
+            prevEnd = min(end, prevEnd)
+        else:
+            prevEnd = end
+    return res
+
+
+print(NonOverlap([[1, 2], [1, 2], [1, 2]]))
