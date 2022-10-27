1f9bb87d866e6271c4b369d5d76e24f667fd1976
Sai <saishashank10@gmail.com>
2022-10-26 21:57:21 -0700

    add

--- NeetCode 150/04. Stack/25. DailyTemperatures.py
+++ NeetCode 150/04. Stack/25. DailyTemperatures.py
@@ -4,7 +4,8 @@ https://leetcode.com/problems/daily-temperatures/
 Input: temperatures = [73,74,75,71,69,72,76,73]
 Output: [1,1,4,2,1,1,0,0]
 
-Approach: we add the temp to a stack. Now if the incoming temp is more than stack temp we pop and
+Approach: we add the temp to a stack. 
+Now if the incoming temp is more than stack temp we pop and
 get the diff of the index. 
 """
 
