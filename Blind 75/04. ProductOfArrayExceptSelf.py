b31fd659ec11cbbc40781297da9e57d1ff4e023b
sun001c <saishashank10@gmail.com>
2023-02-15 21:10:45 -0800

    add

--- Blind 75/04. ProductOfArrayExceptSelf.py
+++ Blind 75/04. ProductOfArrayExceptSelf.py
@@ -1,32 +1,26 @@
 """
 https://leetcode.com/problems/product-of-array-except-self/
 
-Example 1:
-
-Input: nums = [1,2,3,4]
-Output: [24,12,8,6]
-Example 2:
-
-Input: nums = [-1,1,0,-3,3]
-Output: [0,0,9,0,0]
-
-Approach is to iterate through the array from start and then iterate through the array from end.
+APPROACH: 
+for any number, we want to calculate the prefix product and postfix product. 
+iterate through the array from start and then iterate through the array from end.
 [1,2,3,4] after first iteration [1,1,2,6] after second iteration [24,12,8,6]
 
 TC: O(n) we are iterating through the loop
 SC: O(n) we are creating an array 
-"""
 
+FOLLOW-UP: WITHOUT ADDITIONAL SPACE
+"""
 
 def product(nums):
     res = []
     p = 1
     for i in range(len(nums)):
-        res.append(p)
-        p *= nums[i]
+        res.append(p)  # we are calculating prefix product here
+        p *= nums[i]  # update prefix
 
     p = 1
-    for i in range(len(nums)-1, -1, -1):
-        res[i] *= p
-        p *= nums[i]
+    for i in range(len(nums)-1, -1, -1):  # postfix operator here
+        res[i] *= p  # multiply prefix and postfix
+        p *= nums[i]  # update postfix
     return res
--- Blind 75/05. MaxSubArray.py
+++ Blind 75/05. MaxSubArray.py
@@ -1,22 +1,15 @@
 """
-
 https://leetcode.com/problems/maximum-subarray/
-
-Example 1:
-
-Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-Output: 6
-Explanation: [4,-1,2,1] has the largest sum = 6.
 
 Pattern: Sliding window ( as we are moving through the array ), Kadane’s algorithm. 
 we need to iterate through the array and add elements to find the subarray sum. 
-only case the sum of the subarray can be decreasing is the sum goes negative, so if the sum is negative we make it 0.
+only case the sum of the subarray can be decreasing is the sum goes negative, 
+so if the sum is negative we make it 0.
 Follow- up: Divide and conquer ??
 
 TC: we are iterating only once so O(n)
 Sc: O(1) no additional space is required. 
 """
-
 
 def maxSum(nums):
     ans = nums[0]
