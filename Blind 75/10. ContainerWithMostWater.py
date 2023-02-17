53505f627e108fd9d61dce632b252b606f375709
sun001c <saishashank10@gmail.com>
2023-02-17 12:16:30 -0800

    water

--- Blind 75/10. ContainerWithMostWater.py
+++ Blind 75/10. ContainerWithMostWater.py
@@ -1,14 +1,11 @@
 """
 https://leetcode.com/problems/container-with-most-water/
 
-Input: height = [1,8,6,2,5,4,8,3,7]
-Output: 49
-Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. 
-In this case, the max area of water (blue section) the container can contain is 49.
-
 Explanation: we need to find the max area of rectangle. 
 use area formula - length * breadth
 length will be the min of left or right as water will overflow. 
+breadth will be the difference between left and right. 
+breadth is already maximum as left is 0 and right is len-1
 max will be the difference between left and right. 
 and we increase the left and right pointers to try for different lengths and breadths. 
 """
@@ -21,7 +18,7 @@ def maxArea(height):
     ans = 0
     while left < right:  # compute the value after the while loop
         area = (right-left)*min(height[left], height[right])
-        if height[left] < height[right]:  # doubt on why we doing this exactly
+        if height[left] < height[right]:  # doubt on why we doing this exactly - we want to consider best possible length
             left += 1
         else:
             right -= 1
--- NeetCode 150/02. TwoPointers/13. ContainerWithMostWater.py
+++ NeetCode 150/02. TwoPointers/13. ContainerWithMostWater.py
@@ -1,15 +1,10 @@
 """
 https://leetcode.com/problems/container-with-most-water/
 
-Input: height = [1,8,6,2,5,4,8,3,7]
-Output: 49
-Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. 
-In this case, the max area of water (blue section) the container can contain is 49.
-
 Explanation: we need to find the max area of rectangle. 
 use area formula - length * breadth
 length will be the min of left or right as water will overflow. 
-breadth will be the difference between left and right. breadth is already maximum as left is 0 and right is len-1
+
 and, we increase the left and right pointers to get the maximum area. 
 if we increase the pointers based on max length then we'll get the max area.  
 
