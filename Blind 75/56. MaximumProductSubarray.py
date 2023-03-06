532ec19305e393a102dfe6d2ad64487f6869349d
sun001c <saishashank10@gmail.com>
2023-03-06 13:51:05 -0800

    dp

---
+++ Blind 75/56. MaximumProductSubarray.py
@@ -0,0 +1,23 @@
+"""
+https://leetcode.com/problems/maximum-product-subarray/
+
+approach: we need to keep track of every max and min because we might have negative numbers
+so, [3,2,-2,-4] here if we take only positive - 6 but for all it is 48.
+don't know how this is dynamic programming - may be because we are storing every value
+
+Tc: O(n)
+Sc: O(1)
+"""
+
+def func(nums):
+    minp, maxp = 1, 1
+    res = nums[0]
+    for num in nums:
+        if num == 0:
+            minp, maxp = 1, 1
+        temp = maxp * num
+        maxp = max(maxp*num, num, minp*num)
+        minp = min(temp, num, minp*num)
+        res = max(maxp, res)
+    
+    return res
