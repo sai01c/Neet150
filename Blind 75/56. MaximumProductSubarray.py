3deb159f3f20a0c0de2d5eda767062fd09f10adf
sun001c <saishashank10@gmail.com>
2023-03-07 08:35:51 -0800

    dp

--- Blind 75/56. MaximumProductSubarray.py
+++ Blind 75/56. MaximumProductSubarray.py
@@ -3,6 +3,7 @@ https://leetcode.com/problems/maximum-product-subarray/
 
 approach: we need to keep track of every max and min because we might have negative numbers
 so, [3,2,-2,-4] here if we take only positive - 6 but for all it is 48.
+and, if number is 0 product may become 0, just intiate them to 1 again
 don't know how this is dynamic programming - may be because we are storing every value
 
 Tc: O(n)
@@ -13,8 +14,6 @@ def func(nums):
     minp, maxp = 1, 1
     res = nums[0]
     for num in nums:
-        if num == 0:
-            minp, maxp = 1, 1
         temp = maxp * num
         maxp = max(maxp*num, num, minp*num)
         minp = min(temp, num, minp*num)
---
+++ Blind 75/58. LongestConsecutiveSubsequence.py
@@ -0,0 +1,20 @@
+"""
+https://leetcode.com/problems/longest-increasing-subsequence/
+
+Approach: we are going in reverse direction, we initiate the dp
+array with 1 because each one is itself subsequence. for every number
+we add another for loop and check every number
+if the number is greater then we take the max of itself and 1+ that number
+
+Tc: O(n^2)
+Sc: O(n)
+
+"""
+
+def func(nums):
+    dp = [1] * len(nums)
+    for i in range(len(nums)-1, -1, -1):
+        for j in range(i+1, len(nums)):
+            if nums[i] < nums[j]:
+                dp[i] = max(dp[i], 1+dp[j])
+    return max(dp)
