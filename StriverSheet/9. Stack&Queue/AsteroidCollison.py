537ec650aabf202bf99f1ca40f721fa0183034a3
Sai <saishashank10@gmail.com>
2023-04-24 22:13:19 -0700

    Update AsteroidCollison.py

--- StriverSheet/9. Stack&Queue/AsteroidCollison.py
+++ StriverSheet/9. Stack&Queue/AsteroidCollison.py
@@ -1,8 +1,29 @@
 """
 https://leetcode.com/problems/asteroid-collision/
 
-approach
+approach - monotonic stack because we need to compare with previous elements
 
 tc
 sc
 """
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for a in asteroids:
+            stack.append(a)
+            while len(stack) > 1 and stack[-1] < 0 and stack[-2] > 0:
+                if abs(stack[-1]) < stack[-2]:
+                    stack.pop()
+                elif abs(stack[-1]) == stack[-2]:
+                    stack.pop()
+                    stack.pop()
+                elif abs(stack[-1]) > stack[-2]:
+                    val = stack.pop()
+                    stack.pop()
+                    stack.append(val)
+    
+            
+            
+        return stack
+            
