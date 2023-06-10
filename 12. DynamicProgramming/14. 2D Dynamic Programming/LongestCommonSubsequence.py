9084a6b8a5e8a87e4a1b49d3da1b54faf2f90998
Sai <saishashank10@gmail.com>
2023-06-09 21:42:15 -0700

    Update LongestCommonSubsequence.py

--- 12. DynamicProgramming/14. 2D Dynamic Programming/LongestCommonSubsequence.py
+++ 12. DynamicProgramming/14. 2D Dynamic Programming/LongestCommonSubsequence.py
@@ -11,22 +11,30 @@ Sc: O(n)
 
 """
 
-def lcs(text1, text2):
-    rows = len(text1)
-    cols = len(text2)
-    matrix = []
-    for r in range(rows+1):
-        row = []
-        for c in range(cols+1):
-            rows.append(c)
-        matrix.append(row)
-    print(matrix)
-
-    for r in range(rows-1, -1, -1):
-        for c in range(cols-1, -1, -1):
-            if text1[r] == text2[c]:
-                matrix[r][c] = 1 + matrix[r+1][c+1]
-            else:
-                matrix[r][c] = max(matrix[r+1][c], matrix[r][c+1])
-    
-    return matrix[0][0]
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        mat = []
+        rows = len(text1)
+        cols = len(text2)
+        for r in range(rows):
+            temp = []
+            for c in range(cols):
+                temp.append(0)
+            mat.append(temp)
+        
+        for r in range(rows):
+            for c in range(cols):
+                if text1[r] == text2[c]:
+                    if r - 1 >= 0 and c-1 >= 0:
+                        mat[r][c] = 1 + mat[r-1][c-1]
+                    else:
+                        mat[r][c] = 1
+                else:
+                    if r-1 >= 0 and c-1 >= 0:
+                        mat[r][c] = max(mat[r][c-1], mat[r-1][c])
+                    elif r-1 >= 0:
+                        mat[r][c] = max(mat[r-1][c], 0)
+                    elif c-1 >= 0:
+                        mat[r][c] = max(mat[r][c-1], 0)
+        
+        return mat[rows-1][cols-1]
