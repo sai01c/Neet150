54f4324d2b1663c73e72e2b621296c8a6084e1e6
sun001c <saishashank10@gmail.com>
2023-09-03 09:29:27 -0700

    backtracking

--- 09. Heaps/NeetCode150/IPO.py
+++ 09. Heaps/NeetCode150/IPO.py
@@ -1,36 +1,6 @@
 """
 https://leetcode.com/problems/ipo/submissions/
 
-Brute Force approach: I did the same as optimal except I did not use heap for capital.
-
-TLE 33/35 cases.
-
-"""
-
-class Solution:
-    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        have = w 
-        ans = w
-        while k > 0:
-            profit = 0
-            heap = []
-            for i in range(len(capital)):
-                need = capital[i]
-                if have >= need:
-                    heapq.heappush(heap, (- 1 * profits[i], i)) 
-            if heap: 
-                profit, i = heapq.heappop(heap) 
-            else:
-                return ans
-            profits.pop(i)
-            capital.pop(i)
-            profit = -1 * profit
-            ans += (profit)
-            have += profit
-            k -= 1
-        return ans
-    
-"""
 Optimal Approach: have two heaps - one min heap for capital other max heap for profits
 while iterating over the min heap check if our current profit is more than minHeap values. 
 Add all those values which satisfy this condition. Now, out of all the profits available select 
