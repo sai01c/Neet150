3afbf1d1faba24e9cb008c901cdd61ff2e4198f3
sun001c <saishashank10@gmail.com>
2023-10-07 20:56:20 -0700

    dp

--- 05. StackQueue/Striver/CelebrityProblem.py
+++ 05. StackQueue/Striver/CelebrityProblem.py
@@ -3,3 +3,26 @@ https://www.geeksforgeeks.org/the-celebrity-problem/#
 
 TODO
 """
+
+# The knows API is already defined for you.
+# return a bool, whether a knows b
+# def knows(a: int, b: int) -> bool:
+
+class Solution:
+    def findCelebrity(self, n: int) -> int:
+        candidate = 0
+        for member in range(n):
+            if knows(candidate, member):
+                #celebrity should not know anyone so if we this returns True > our candidate can not be celebrity anymore
+                candidate = member
+            
+        #we will get candidate where it does not know member
+        for member in range(n):
+            if (member != candidate) and (
+                (
+                    not knows(member, candidate)) or knows(candidate, member)
+                ):
+                return -1
+        
+        return candidate
+                
--- 12. DynamicProgramming/1D Dynamic Programming/DecodeWays.py
+++ 12. DynamicProgramming/1D Dynamic Programming/DecodeWays.py
@@ -24,3 +24,20 @@ def decodeWays(s):
             dp[i] += dp[i+2]
         
     return dp[0]
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp={}
+        def dfs(i): 
+            if i==len(s): 
+                return 1 
+            if s[i]=="0": 
+                return 0 
+            if i in dp: 
+                return dp[i]
+            ans = dfs(i+1) 
+            if (i+1<len(s) and (s[i]=="1" or s[i]=="2" and s[i+1] in "0123456")): 
+                ans += dfs(i+2)
+            dp[i]=ans
+            return ans 
+        return dfs(0) 
