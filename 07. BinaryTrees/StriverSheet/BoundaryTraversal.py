d585c054a2858412fb07044bc5413d1af7847fc4
sun001c <saishashank10@gmail.com>
2023-10-08 19:54:47 -0700

    trees

--- 07. BinaryTrees/StriverSheet/BoundaryTraversal.py
+++ 07. BinaryTrees/StriverSheet/BoundaryTraversal.py
@@ -1,3 +1,47 @@
 """
 TODO
 """
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
+        
+        def leftDFS(node):
+            if node:
+                if not node.left and not node.right:
+                    return []
+                else:
+                    if node.left:
+                        return [node.val] + leftDFS(node.left)
+                    else:
+                        return [node.val] + leftDFS(node.right)
+            else:
+                return []
+            
+        def rightDFS(node):
+            if node:
+                if not node.left and not node.right:
+                    return []
+                else:
+                    if node.right:
+                        return rightDFS(node.right) + [node.val]
+                    else:
+                        return rightDFS(node.left) + [node.val]
+            else:
+                return []
+        
+        def leaves(node):
+            if node:
+                if not node.left and not node.right:
+                    return [node.val]
+                else:
+                    return leaves(node.left) + leaves(node.right)
+            else:
+                return []
+        
+        return [root.val] + leftDFS(root.left) + leaves(root.left) + leaves(root.right) + rightDFS(root.right)
