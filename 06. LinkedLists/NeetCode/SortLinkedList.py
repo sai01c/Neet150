e0b1ac798df2cf50a057dac9c198fc3a932d2b5c
Sai <saishashank10@gmail.com>
2023-10-23 17:52:33 -0700

    Update SortLinkedList.py

--- 06. LinkedLists/NeetCode/SortLinkedList.py
+++ 06. LinkedLists/NeetCode/SortLinkedList.py
@@ -1,26 +1,40 @@
 """
 https://leetcode.com/problems/sort-list/
 
-approach - first sort the linked list by copying values to an array
-after sorting the array, use two pointers to create linked list and join them
-
-tc - nlogn
-sc - n
 """
 
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        nums = []
-        dummy = ListNode(0, head)
-        curr = head
-        while curr:
-            nums.append(curr.val)
+        # Base Case: If the length of the linked list is less than or equal to 1, then the list is already sorted
+        if not head or not head.next:
+            return head
+
+        # Split the linked list into two halves using "slow and fast pointer" technique to find the midpoint of the linked list
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        # The midpoint of the linked list is slow.next
+        mid = slow.next
+        # Set slow.next to None to separate the left and right halves of the linked list
+        slow.next = None
+
+        # Recursively sort the left and right halves of the linked list
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        # Merge the two sorted halves of the linked list
+        dummy = ListNode(0)
+        curr = dummy
+        while left and right:
+            if left.val < right.val:
+                curr.next = left
+                left = left.next
+            else:
+                curr.next = right
+                right = right.next
             curr = curr.next
-        curr2 = head
-        i = 0
-        nums.sort()
-        while curr2 and i < len(nums):
-            curr2.val = nums[i]
-            i += 1
-            curr2 = curr2.next
+        # Append the remaining nodes of the left or right half to the end of the sorted list
+        curr.next = left or right
+
         return dummy.next
