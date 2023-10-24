316b2f4b38835f9938d46ec7881d09153d5008f2
sun001c <saishashank10@gmail.com>
2023-10-23 17:43:44 -0700

    changes

--- Console.py
+++ Console.py
@@ -1,3 +1,23 @@
-print(ord('a')- ord('a'))
 
-print('ab' * 2)
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverse(self, head: ListNode, target: int):
+        prev = None
+        curr = head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+            if prev.val == target:
+                head.next = curr
+                return prev
+
+obj = Solution()       
+result = obj.reverse(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))), 3)
+
+print(result)
