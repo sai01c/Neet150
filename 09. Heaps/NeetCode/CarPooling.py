370dbe7bbf9c6ad4f50faeefbe688aee41745bff
sun001c <saishashank10@gmail.com>
2023-10-23 23:12:00 -0700

    heap

--- 09. Heaps/NeetCode/CarPooling.py
+++ 09. Heaps/NeetCode/CarPooling.py
@@ -2,3 +2,18 @@
 https://leetcode.com/problems/car-pooling/
 
 """
+
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        points = [0] * 1001
+        
+        for cap, start, end in trips:
+            for i in range(start, end):
+                points[i] += cap
+            
+                if points[i] > capacity:
+                    return False
+                
+        return True
+            
+            
